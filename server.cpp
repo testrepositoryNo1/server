e3d64708e37ef0c795f9675f4f51c5b916cff87a
Sergey Masuryan <smasuryan@plgn.ru>
2016-12-26 10:40:06 +0500

    date time server

--- server.cpp
+++ server.cpp
@@ -16,51 +16,51 @@ const int port = 3425;
 
 int main()
 {
-    int sTimeClient, /* дискриптор клиентского сокета */
-        sTimeServer; /* дискриптор серверного сокета */
-    struct sockaddr_in addr;
-    time_t currentTime;
-    ofstream fout;
+      int sTimeClient, /* дискриптор клиентского сокета */
+          sTimeServer; /* дискриптор серверного сокета */
+      sockaddr_in addr;
+      time_t currentTime;
+      ofstream fout;
 
-    sTimeServer = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if(sTimeServer < 0)
-    {
-        perror("socket");
-        return -1;
-    }
+      sTimeServer = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    if(bind(sTimeServer, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-        perror("bind");
-        return -2;
-    }
-
-    listen(sTimeServer, 1);
+      if(sTimeServer < 0) {
+              perror("socket");
+              return -1;
+          }
 
 
-    while(1)
-    {
-            fout.open("test.log", ios_base::app | ios_base::out);
-            sTimeClient = accept(sTimeServer, NULL, NULL);
-            if(sTimeClient < 0) {
-                    perror("accept");
-                    return -3;
-                }
-            currentTime = time(NULL);
-            send(sTimeClient, (void *) currentTime, sizeof(currentTime), 0);
+      addr.sin_family = AF_INET;
+      addr.sin_port = htons(port);
+      addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-            cout << "request at: " << ctime(&currentTime);
-            fout << "request at: " << ctime(&currentTime);
+      if(bind(sTimeServer, (sockaddr *)&addr, sizeof(addr)) < 0) {
+              perror("bind");
+              return -2;
+          }
+
+      listen(sTimeServer, 20);
 
 
-            close(sTimeClient);
-            fout.close();
-        }
+      while(1) {
+              fout.open("test.log", ios_base::app | ios_base::out);
 
-    close(sTimeServer);
+              sTimeClient = accept(sTimeServer, NULL, NULL);
 
-    return 0;
+              if(sTimeClient < 0) {
+                      perror("accept");
+                      return -3;
+                  }
+              currentTime = time(NULL);
+              send(sTimeClient, (void *) currentTime, sizeof(currentTime), 0);
+              cout << "request at: " << ctime(&currentTime);
+              fout << "request at: " << ctime(&currentTime);
+
+              close(sTimeClient);
+              fout.close();
+          }
+
+      close(sTimeServer);
+
+      return 0;
 }
